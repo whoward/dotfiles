8e5fd48fb18c81b3b593a78e83b0508d853397e3
William Howard <whoward@leadformance.com>
2011-01-21 19:36:30 -0500

    much improved ps1

--- ps1.bash
+++ ps1.bash
@@ -1,1 +1,64 @@
-export PS1='\[\033[01;34m\]\u\[\033[00m\]@\[\033[00;32m\]\h\[\033[00m\]\[\033[00;31m\]$(__git_ps1 "[%s]")\[\033[00m\]:\[\033[01;30m\]\w\[\033[00m\]\$ '
+
+function __git_dirty {
+  git diff --quiet HEAD &>/dev/null 
+  [ $? == 1 ] && echo "!"
+}
+
+function __git_branch {
+  __git_ps1 "|%s"
+}
+
+function __my_rvm_ruby_version {
+  local gemset=$(echo $GEM_HOME | awk -F'@' '{print $2}')
+  local version=$(echo $MY_RUBY_HOME | awk -F'.rvm/rubies/' '{print $2}')
+  local full="$version$gemset"
+  [ "$full" != "" ] && echo "$full"
+}
+
+bash_prompt() {
+  local NONE="\[\033[0m\]"    # unsets color to term's fg color
+
+  # bolding in the current color
+  local BOLD="\[\033[1m\]"
+  local BOLDOFF="\[\033[22m\]"
+
+  # regular colors
+  local K="\[\033[0;30m\]"    # black
+  local R="\[\033[0;31m\]"    # red
+  local G="\[\033[0;32m\]"    # green
+  local Y="\[\033[0;33m\]"    # yellow
+  local B="\[\033[0;34m\]"    # blue
+  local M="\[\033[0;35m\]"    # magenta
+  local C="\[\033[0;36m\]"    # cyan
+  local W="\[\033[0;37m\]"    # white
+
+  # emphasized (bolded) colors
+  local EMK="\[\033[1;30m\]"
+  local EMR="\[\033[1;31m\]"
+  local EMG="\[\033[1;32m\]"
+  local EMY="\[\033[1;33m\]"
+  local EMB="\[\033[1;34m\]"
+  local EMM="\[\033[1;35m\]"
+  local EMC="\[\033[1;36m\]"
+  local EMW="\[\033[1;37m\]"
+
+  # background colors
+  local BGK="\[\033[40m\]"
+  local BGR="\[\033[41m\]"
+  local BGG="\[\033[42m\]"
+  local BGY="\[\033[43m\]"
+  local BGB="\[\033[44m\]"
+  local BGM="\[\033[45m\]"
+  local BGC="\[\033[46m\]"
+  local BGW="\[\033[47m\]"
+
+  local UC=$W                 # user's color
+  [ $UID -eq "0" ] && UC=$R   # root's color
+
+  PS1="$EMB\u$Y@$EMY\h$Y[$(__my_rvm_ruby_version)$BOLD$(__git_branch)$BOLDOFF]$K:$EMK\w$ $W"
+
+#  PS1="$B\$(__my_rvm_ruby_version)$Y\h$W:$EMY\w$EMW\$(__git_branch)$EMY\$(__git_dirty)${NONE} $ "
+}
+
+bash_prompt
+unset bash_prompt
